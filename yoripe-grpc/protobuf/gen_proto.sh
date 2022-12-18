3ef3a3d5c5d744f3a2731f3dd907d53d3a8c9b0a
NCanggoro <kolisnetral@gmail.com>
2022-12-19 06:27:14 +0700

    add creating folder build for generated protobuf

--- yoripe-grpc/protobuf/gen_proto.sh
+++ yoripe-grpc/protobuf/gen_proto.sh
@@ -1,2 +1,3 @@
+mkdir build &&
 protoc --proto_path=src --php_out=build --grpc_out=build --plugin=protoc-gen-grpc=/usr/bin/protoc-gen-php-grpc src/*.proto &&
 protoc --proto_path=src --php_out=build --grpc_out=build  --plugin=protoc-gen-grpc=/usr/bin/grpc_php_plugin src/*.proto
